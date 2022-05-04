3285eb4de9cec3d620d40ef11b89264d67e7b0d5
iarlaithc <iarlaithcosgrove@gmail.com>
2022-05-04 10:23:00 +0100

    Add project files.

---
+++ MyMVVMProject/Model/HelloWorldModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMVVMProject.Model
+{
+    public class HelloWorldModel
+    {
+        private List<string> repositoryData;
+        public string ImportantInfo
+        {
+            get 
+            {
+                return ConcatenateData(repositoryData);
+            }
+        }
+        public HelloWorldModel()
+        {
+            repositoryData = GetData();
+        }
+
+        private List<string> GetData()
+        {
+            repositoryData = new List<string>()
+            {
+                "Hello",
+                "World"
+            };
+            return repositoryData;
+        }
+        
+        private string ConcatenateData(List<string> dataList)
+        {
+            string importantInfo = dataList.ElementAt(0) + "," + dataList.ElementAt(1) + "!";
+            return importantInfo;
+        }
+
+    }
+}
---
+++ MyMVVMProject/ViewModel/HelloWorldViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MyMVVMProject.Model;
+
+namespace MyMVVMProject.ViewModel
+{
+    //implements iNotifyPropertyChanged interface to support bindings
+    public class HelloWorldViewModel
+    {
+        private string helloString;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HelloString
+        {
+            get
+            {
+                return helloString;
+            }
+            set
+            {
+                helloString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //Raises OnPropertychangedEvent when property changes
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public HelloWorldViewModel()
+        {
+            HelloWorldModel helloWorldModel = new HelloWorldModel();
+            helloString = helloWorldModel.ImportantInfo;
+        }
+    }
+}
